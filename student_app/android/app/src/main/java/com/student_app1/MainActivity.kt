6a1e69ae0882b936b9fd77c2b36e665c1e8e0bd6
mallikera-spec <chandanmallik@gmail.com>
2026-07-14 09:28:48 +0530

    feat: rename student app package to student_app1 and configure release signing configs for both apps

--- student_app/android/app/src/main/java/com/student_app1/MainActivity.kt
+++ student_app/android/app/src/main/java/com/student_app1/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.student_app
+package com.student_app1
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- student_app/android/app/src/main/java/com/student_app1/MainApplication.kt
+++ student_app/android/app/src/main/java/com/student_app1/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.student_app
+package com.student_app1
 
 import android.app.Application
 import com.facebook.react.PackageList
